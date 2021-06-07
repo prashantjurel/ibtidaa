4a56608b450e3fc5d01d10e4b457036ff6fd4b98
prashantjurel <prashantjurel@outlook.com>
2021-06-07 10:49:08 +0530

    Setting notification manager

--- app/src/main/java/com/prashant/ibtidaa/exoPlayer/MusicService.kt
+++ app/src/main/java/com/prashant/ibtidaa/exoPlayer/MusicService.kt
@@ -7,6 +7,7 @@ import android.support.v4.media.session.MediaSessionCompat
 import androidx.media.MediaBrowserServiceCompat
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
+import com.prashant.ibtidaa.exoPlayer.callbacks.MusicPlayerNotificationListener
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -22,27 +23,41 @@ class MusicService : MediaBrowserServiceCompat() {
     @Inject
     lateinit var exoPlayer: SimpleExoPlayer
 
+    private lateinit var musicNotificationManager: MusicNotificationManager
+
     private val serviceJob = Job()
     private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
 
     private lateinit var mediaSession: MediaSessionCompat
     private lateinit var mediaSessionConnector: MediaSessionConnector
 
+    var isForegroundService = false
+
     override fun onCreate() {
         super.onCreate()
         val activityIntent = packageManager?.getLaunchIntentForPackage(packageName)?.let {
-            PendingIntent.getActivity(this,0,it,0)
+            PendingIntent.getActivity(this, 0, it, 0)
         }
 
-        mediaSession = MediaSessionCompat(this,SERVICE_TAG).apply {
+        mediaSession = MediaSessionCompat(this, SERVICE_TAG).apply {
             setSessionActivity(activityIntent)
             isActive = true
         }
 
-        sessionToken  = mediaSession.sessionToken
+        sessionToken = mediaSession.sessionToken
+
+        musicNotificationManager = MusicNotificationManager(
+            this,
+            mediaSession.sessionToken,
+            MusicPlayerNotificationListener(this)
+        ) {
+
+        }
+
         mediaSessionConnector = MediaSessionConnector(mediaSession)
         mediaSessionConnector.setPlayer(exoPlayer)
     }
+
 
     override fun onDestroy() {
         super.onDestroy()
--- app/src/main/java/com/prashant/ibtidaa/other/Constants.kt
+++ app/src/main/java/com/prashant/ibtidaa/other/Constants.kt
@@ -1,5 +1,7 @@
 package com.prashant.ibtidaa.other
 
 object Constants {
-    const val EPISODE_COLLECTION = "episodes"
+    const val SONG_COLLECTION = "songs"
+    const val NOTIFICATION_CHANNEL_ID = "music"
+    const val NOTIFICATION_ID = 1
 }
